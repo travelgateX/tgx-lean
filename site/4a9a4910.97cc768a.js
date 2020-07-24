2dd645255d3a10a4e070407f8702cbe59f993118
nicolasgarfinkiel <nicolasgarfinkiel@users.noreply.github.com>
2020-07-24 15:36:35 +0000

    Site publish after commit to gh-pages

---
+++ site/4a9a4910.97cc768a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{146:function(e){e.exports=JSON.parse('{"permalink":"/tgx-lean/site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null}')}}]);
