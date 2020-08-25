b655d0f5a59032ecf728f255db11abd938929659
operezfuentes <operezfuentes@users.noreply.github.com>
2020-08-25 15:49:46 +0000

    Site publish after commit to gh-pages

--- site/4a9a4910.ed36628a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{147:function(e){e.exports=JSON.parse('{"permalink":"/tgx-lean/site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null}')}}]);
